be7c55cb14ec3516b01ec0520d69d8ae9f89997c
Magamou Gueye <gueyemagamou@gmail.com>
2022-05-16 18:55:47 +0000

    integrated in map componenet

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'bingmaps';
+import { initialize, whenLoaded } from 'bing-maps-loader';
 
 @Component({
   selector: 'app-maps',
@@ -6,8 +8,36 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./maps.component.css']
 })
 export class MapsComponent implements OnInit {
+  constructor() {
+    const API_KEY =
+      'Ams581aR0adsR3ZshnyTnH9n9-Ua68q_WetLVX483Icc3j-d6n_QSfHDVYzitfGB';
+    initialize(API_KEY);
+  }
+  addPinToNewMap() {
+    // whenLoaded will resolve when the Map library is loaded
+    whenLoaded.then(() => {
+      const map=new Microsoft.Maps.Map('#map', {
+        center: new Microsoft.Maps.Location(
+          14.818551984704184, -17.252053256597538
+        ),
+      }); // <-- can also use references e.g. Vue $refs, React.createRef()
+      this.addElement(map);
+    });
+  }
+  addElement(_map:Microsoft.Maps.Map):void{
+        var center=_map.getCenter();
+        console.log("center:", center);
+      //create custom Pushpin
+      var pin=new Microsoft.Maps.Pushpin(center, {
+          title:'mg',
+          subTitle:'',
+          text:''
+      });
+      _map.entities.push(pin); 
+  }
   ngOnInit(): void {
-    
+    this.addPinToNewMap();
+    // this.addElement();
   }
 
 }
--- src/app/services/map/map.service.ts
+++ src/app/services/map/map.service.ts
@@ -1,36 +1,9 @@
 import { Injectable } from '@angular/core';
 
-const url = 'http://www.bing.com/api/maps/mapcontrol?branch=release';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
-  private static promise:any;
-  public static load(): any {
-    // First time 'load' is called?
-    if (!MapService.promise) {
-
-        // Make promise to load
-        MapService.promise = new Promise( resolve => {
-
-            // Set callback for when bing maps is loaded.
-            // window['__onBingLoaded'] = (ev: any) => {
-            //     resolve('Bing Maps API loaded');
-            // };
-            window.addEventListener('load', (e)=>{
-              this.load();
-            })
-           
-            const node = document.createElement('script');
-            node.src = url;
-            node.type = 'text/javascript';
-            node.async = true;
-            node.defer = true;
-            document.getElementsByTagName('head')[0].appendChild(node);
-        });
-    }
-
-    // Always return promise. When 'load' is called many times, the promise is already resolved.
-    return MapService.promise;
-}
+  
 }
